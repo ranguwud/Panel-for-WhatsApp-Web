f1059d62ff43ea6c0b76c1143c1980d4e7ff1f3c
Ranguwud <ranguwud@gmail.com>
2017-11-08 22:49:36 +0100

    Enabled loading of WhatsApp Web in background iframe

---
+++ js/background.js
@@ -0,0 +1,11 @@
+browser.webRequest.onHeadersReceived.addListener(function(info) {
+    if (info.tabId > -1) return;
+    var headers = info.responseHeaders;
+    for (var i = 0; i < headers.length; i++) {
+        var name = headers[i].name.toLowerCase();
+        if (name === 'x-frame-options' || name === 'frame-options') {
+            headers.splice(i, 1);
+            return {"responseHeaders": headers};
+        }
+    }
+}, {"urls": ["*://*.web.whatsapp.com/*"]}, ["blocking", "responseHeaders"]);
