8dfea5a26aea955eda2d3dc7c66b3f97f981c472
Ranguwud <ranguwud@gmail.com>
2017-11-08 21:31:22 +0100

    Use relative selectors to apply MutationObservers in inject.js

--- js/inject.js
+++ js/inject.js
@@ -1,5 +1,3 @@
-var target = document.body;
-
 var chatObserver = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
         console.log(mutation.target.className);
@@ -7,24 +5,39 @@ var chatObserver = new MutationObserver(function(mutations) {
 });
 
 var appWrapperObserver = new MutationObserver(function(mutations, observer) {
-    mutations[0].target.querySelectorAll('div.chat').forEach(function(node) {
-        chatObserver.observe(node, {attributes: true});
+    mutations.some(function(mutation) {
+        targets = mutation.target.querySelectorAll('div.chat');
+        if (targets.length > 0) {
+            targets.forEach(function(target) {
+                chatObserver.observe(target, {attributes: true});
+            });
+            observer.disconnect();
+            return true;
+        }
     });
 });
 
-var appObserver = new MutationObserver(function(mutations) {
-    mutations.forEach(function(mutuation) {
-        target=document.body.querySelector('div#app').querySelector('div.app-wrapper');
-        appWrapperObserver.observe(target, {childList: true})
+var appObserver = new MutationObserver(function(mutations, observer) {
+    mutations.some(function(mutation) {
+        target = mutation.target.querySelector('div.app-wrapper');
+        if (target) {
+            appWrapperObserver.observe(target, {childList: true})
+            observer.disconnect();
+            return true;
+        }
     });
 });
 
-var bodyObserver = new MutationObserver(function(mutations) {
-    mutations.forEach(function(mutation) {
-        target = document.body.querySelector('div#app');
-        appObserver.observe(target, {childList: true});
+var bodyObserver = new MutationObserver(function(mutations, observer) {
+    mutations.some(function(mutation) {
+        target = mutation.target.querySelector('div#app');
+        if (target) {
+            appObserver.observe(target, {childList: true});
+            observer.disconnect();
+            return true;
+        }
     });
 });
 
+var target = document.body;
 bodyObserver.observe(target, {childList: true});
-
